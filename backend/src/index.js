9d615d879ce73279927d1be8c3057ad558c46c51
Ivan Fomenko <ivan.fmn@yandex.ru>
2020-11-26 18:40:08 +0300

    update index

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,5 @@
-import express from 'express'
-import connectModules from './connectModules'
+const app = require('./connectModules')
 
-const app = express()
-
-app.use(connectModules)
 app.listen(process.env.SERVER_PORT, ()=>{
     const date = new Date()
     console.log(`Server start | ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}  ${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`)
