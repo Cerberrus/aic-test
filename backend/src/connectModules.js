f4c52c08a62268349e6dfc9df3bad1c5ff37fb71
Ivan Fomenko <ivan.fmn@yandex.ru>
2020-11-26 18:38:41 +0300

    Update connection within modules

--- backend/src/connectModules.js
+++ backend/src/connectModules.js
@@ -1,26 +1,25 @@
-import express          from 'express'
-import dotenv           from 'dotenv'
-import cookieParser     from 'cookie-parser'
-import bodyParser       from 'body-parser'
-import authRouter       from './components/router_controller_model/auth/router'
-import coordinateRouter from './components/router_controller_model/coordinate/router'
-import jobRequestRouter from './components/router_controller_model/job/request/router'
-import jobVacancyRouter from './components/router_controller_model/job/vacancy/router'
-import settingRouter    from './components/router_controller_model/settings/router'
+const express          = require('express')
+const dotenv           = require('dotenv')
+const cookieParser     = require('cookie-parser')
+const bodyParser       = require('body-parser')
+const authRouter       = require('./components/router_controller_model/auth/router')
+const coordinateRouter = require('./components/router_controller_model/coordinate/router')
+const jobRequestRouter = require('./components/router_controller_model/job/request/router')
+const jobVacancyRouter = require('./components/router_controller_model/job/vacancy/router')
+const settingRouter = require('./components/router_controller_model/settings/router')
 
 dotenv.config()                 // connect .env file and use them variables
 
 const app = express()
+const router = express.Router()
 
-
+app.use(bodyParser.json())        // use for get information from body
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(cookieParser(process.env.COOKIE_SECURE_KEY))         // use for get and set information from cookie
 app.use(authRouter)             //
 app.use(coordinateRouter)       //
 app.use(jobRequestRouter)       // include all routes
 app.use(jobVacancyRouter)       //
 app.use(settingRouter)          //
-
-app.use(bodyParser.json)        // use for get information from body
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(cookieParser())         // use for get and set information from cookie
 
 module.exports = app
