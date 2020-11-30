c8d5190f7fd3f4c26e82253a8b4cfd0dc51a78b3
Ivan Fomenko <ivan.fmn@yandex.ru>
2020-11-30 17:06:57 +0300

    Implement some modules

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const app = require('./connectModules')
+const workers = require('./components/workers/Workers')
+// const admin = require('./components/router_controller_model/auth/Authentication')
+//
+// admin.postAdmin("root", 'root', 'root')
 
-app.listen(process.env.SERVER_PORT, ()=>{
+app.listen(process.env.SERVER_PORT, async ()=>{
+    await workers.initWorkers([{path:'C:\\GitHub\\aic-test\\backend\\src\\components\\image\\ImageConverterWorker.js', count:1}])
     const date = new Date()
     console.log(`Server start | ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}  ${date.getDay()}-${date.getMonth()}-${date.getFullYear()}`)
 })
