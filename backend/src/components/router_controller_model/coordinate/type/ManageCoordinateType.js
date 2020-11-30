17aa42837898c4eb1f109f9910352e0beb406733
Ivan Fomenko <ivan.fmn@yandex.ru>
2020-11-30 16:59:35 +0300

    Update coordinate module

--- backend/src/components/router_controller_model/coordinate/type/ManageCoordinateType.js
+++ backend/src/components/router_controller_model/coordinate/type/ManageCoordinateType.js
@@ -8,12 +8,12 @@ const postCoordinateType=async ({name})=>{
     await connection.execute('insert into coordinate_type(name) values(?)', [name])
     return getCoordinateTypeList()
 }
-const updateCoordinateType=async ({id}, {name})=>{
-    await connection.execute('update coordinate_type set coordinate=?,name=?, description=?, typeId=? where id =?', [name, id])
-    return getCoordinateTypeList()
-}
+// const updateCoordinateType=async ({id}, {name})=>{
+//     await connection.execute('update coordinate_type set coordinate=?,name=?, description=?, typeId=? where id =?', [name, id])
+//     return getCoordinateTypeList()
+// }
 const deleteCoordinateType=async ({id})=>{
     await connection.execute('delete from coordinate_type where id=?)', [id])
     return getCoordinateTypeList()
 }
-module.exports = {getCoordinateTypeList, postCoordinateType, updateCoordinateType, deleteCoordinateType}
+module.exports = {getCoordinateTypeList, postCoordinateType, deleteCoordinateType}
