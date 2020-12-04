e9ad6b2040c68a89416940db6d4394db5f9e9c37
Ivan Fomenko <ivan.fmn@yandex.ru>
2020-12-04 15:25:29 +0300

    Update config

--- client/config/client.js
+++ client/config/client.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { render, hydrate } from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import { loadableReady } from '@loadable/component'
 import reducers from '~user/reducers'
 
-import App from '~user/components/app/App'
+import AppUser from '~user/components/app/App'
+import AppAdmin from '~admin/components/app/App'
 
 const {externals: {isDev}} = require('./webpack/base.config')
 const store = createStore(reducers, { ...window.APP_STATE })
@@ -14,7 +15,10 @@ const store = createStore(reducers, { ...window.APP_STATE })
 const content = [
     <BrowserRouter>
         <Provider store={store}>
-            <App/>
+            <Switch>
+                <Route       path="/admin" component={AppAdmin} />
+                <Route       path="/*"     component={AppUser} />
+            </Switch>
         </Provider>
     </BrowserRouter>
     ,document.getElementById('app')
