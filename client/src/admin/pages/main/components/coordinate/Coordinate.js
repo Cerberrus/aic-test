bb1764421450baca88df96fa0e2d47e684530f64
Cerberrus <genochka_kravchenko@mail.ru>
2020-12-14 11:48:13 +0300

    Update coordinate admin component

--- client/src/admin/pages/main/components/coordinate/Coordinate.js
+++ client/src/admin/pages/main/components/coordinate/Coordinate.js
@@ -1,79 +1,69 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import axios    from "axios"
 
+//Import static files
 import './Coordinate.css'
-import axios from "axios";
 
-export default class Navigation extends Component{
-
+export default class Navigation extends Component {
     state = {
-        coordinateList: [],
-        typeList: []
+        coordinateList: []
     }
 
     componentDidMount() {
         this.getCoordinate()
     }
 
-    getCoordinate=()=>{
+    getCoordinate = () => {
         axios({
             method: 'get',
             url: process.env.API_BASE + '/coordinate',
             withCredentials: true
         })
-            .then((response)=>{
-                console.log(response)
+            .then((response) => {
                 this.setState({
-                    coordinateList:response.data.coordinateList,
-                    typeList:response.data.typeList
+                    coordinateList: response.data.coordinateList
                 })
             })
             .catch((error)=>{
                 console.log('error')
             })
     }
-    render(){
-        let coordinateList
-        if('features' in this.state.coordinateList){
-            coordinateList = this.state.coordinateList.features.map((item)=>(
-                <div className="coordinate__card" key={item.id}>
-                    <p className="coordinate__type">{item.properties.typeTitle}</p>
-                    <p className="coordinate__cords">{item.geometry.coordinates[0]} | {item.geometry.coordinates[1]}</p>
-                    <p className="coordinate__designation">{item.title}</p>
-                    <div className="">
-                        <img src="https://aic.xutd.tk/static/icons/edit.svg" className="coordinate__img" alt=""/>
-                        <img src="https://aic.xutd.tk/static/icons/hide.svg" className="coordinate__img" alt=""/>
-                        <img src="https://aic.xutd.tk/static/icons/close.svg" className="coordinate__img" alt=""/>
-                    </div>
-                </div>
-            ))
-        }
-        else {
-            coordinateList = <div className="coordinate__card">
-                <p className="coordinate__type">Загрузка</p>
-                <p className="coordinate__cords">Загрузка</p>
-                <p className="coordinate__designation">Загрузка</p>
-                <div className="">
-                    <img src="https://aic.xutd.tk/static/icons/edit.svg" className="coordinate__img" alt=""/>
-                    <img src="https://aic.xutd.tk/static/icons/hide.svg" className="coordinate__img" alt=""/>
-                    <img src="https://aic.xutd.tk/static/icons/close.svg" className="coordinate__img" alt=""/>
-                </div>
-            </div>
-        }
+    render() {
+        const { features } = this.state.coordinateList
 
         return(
             <section className="coordinate">
                 <div className="coordinate__top">
                     <h1 className="title coordinate__title">Координаты</h1>
-                    <button className="button_yellow">Добавить</button>
+                    <Link to='' className="button button_yellow">Добавить</Link>
                 </div>
                 <div className="coordinate__tableHeader">
                     <p className="coordinate__item">Тип</p>
                     <p className="coordinate__item">Координаты</p>
                     <p className="coordinate__item">Название</p>
                 </div>
-                <div>
-                    {coordinateList}
-                </div>
+                <ul>
+                    {features && features.map((item) => (
+                        <li className="coordinate__card" key={item.id}>
+                            <p className="coordinate__type">{item.properties.typeTitle}</p>
+                            <p className="coordinate__cords">{item.geometry.coordinates[0]} | {item.geometry.coordinates[1]}</p>
+                            <p className="coordinate__designation">{item.title}</p>
+                            <ul className="coordinate__cardList">
+                                <li>
+                                    <Link to="" title="Редактировать">
+                                        <img src="https://aic.xutd.tk/static/icons/edit.svg"  className="coordinate__img" alt=""/>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <button title="Удалить">
+                                        <img src="https://aic.xutd.tk/static/icons/close.svg" className="coordinate__img" alt=""/>
+                                    </button>
+                                </li>
+                            </ul>
+                        </li>
+                    ))}
+                </ul>
             </section>
         )
     }
