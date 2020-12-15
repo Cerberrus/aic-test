a2d5e25482849fc7e1d1407c169743edcd939e6a
Cerberrus <genochka_kravchenko@mail.ru>
2020-12-15 17:52:10 +0300

    Updated admin setting component

--- client/src/admin/pages/main/components/setting/Setting.js
+++ client/src/admin/pages/main/components/setting/Setting.js
@@ -1,52 +1,110 @@
 import React, { Component } from "react"
-import { Link } from "react-router-dom"
+import {Link, Redirect} from "react-router-dom"
+
+import Loader from "~admin/components/loader/Loader"
 
 // Import static files
 import './Setting.css'
 
-export default class Setting extends Component {
-    state = {
+import model from "~src/model/model"
 
+export default class Setting extends Component {
+    model = new model()
+
+    state = {
+        settings: {},
+        loading:  true
+    }
+
+    componentDidMount() {
+        this.model.getSettings()
+            .then((settings) => {
+                this.setState({
+                    settings: settings,
+                    loading:  false,
+                })
+            })
+    }
+
+    setFiled = (e) => {
+        const key      = e.target.name
+        const value    = e.target.value
+        const settings = this.state.settings
+
+        settings[key]  = value
+        this.setState({settings})
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault()
+        this.model.postSettings(this.state.settings)
+            .then((response) => {
+                console.log(response);
+            })
     }
 
     render(){
+        const { loading, settings } = this.state
+
+        if (loading) {
+            return <Loader/>
+        }
+
         return(
             <>
-                <form className="setting">
-                    <h1 className="title">Настройки</h1>
-                    <div className="setting__group">
-                        <p>Логотип</p>
-                        <div className="setting__group">
-                            <label className="input__label">
-                                <img src=" image/iconmonstr-paperclip-1%201.svg" className=" form__img" alt=""/>
-                                выберете или перетащите файл
-                                <input type=" file" className=" input__file"/>
-                            </label>
-                        </div>
-                    </div>
-
+                <h1 className="title">Настройки</h1>
+                <form className="setting" onSubmit={this.onSubmit}>
                     <label>
                         <span>Контактый телефон *</span>
                         <input
                             className="form__input"
                             type=" tel"
                             placeholder="+7 ("
-                            name=""
+                            name="phone"
+                            defaultValue={settings.phone}
+                            onChange={this.setFiled}
                         />
                     </label>
 
                     <label>
-                        <span>Ваш Instagram аккаунт</span>
+                        <span>Логин Instagram аккаунта</span>
                         <input
                             className="form__input"
                             type="text"
-                            placeholder="Instagram"
-                            name=""
+                            placeholder="login"
+                            name="instLogin"
+                            defaultValue={settings.instLogin}
+                            onChange={this.setFiled}
                         />
                     </label>
 
-                    <button className="setting__button button_yellow">Сохранить</button>
-                    <Link to="/admin/coordinate" className="setting__button button button_gray">Отменить</Link>
+                    <label>
+                        <span>Пароль Instagram аккаунта</span>
+                        <input
+                            className="form__input"
+                            type="text"
+                            placeholder="password"
+                            name="instPassword"
+                            defaultValue={settings.instPassword}
+                            onChange={this.setFiled}
+                        />
+                    </label>
+
+                    <div className="setting__buttonGroup">
+                        <button
+                            type="submit"
+                            className="setting__button button_yellow"
+                        >
+                            Сохранить
+                        </button>
+                        <button
+                            type="button"
+                            className="setting__button button button_gray"
+                            onClick={() => window.location.reload()}
+                        >
+                            Отменить
+                        </button>
+                    </div>
                 </form>
             </>
         )
