75a6eb277c766b52b34885b2fb21a914771f4843
Cerberrus <genochka_kravchenko@mail.ru>
2020-12-19 01:52:50 +0300

    Added open graph markup

--- client/src/user/components/app/App.js
+++ client/src/user/components/app/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Switch, Route } from "react-router-dom"
+import {Switch, Route, useLocation} from "react-router-dom"
 import { AnimatePresence } from "framer-motion"
 import Helmet   from "react-helmet"
 import loadable from "@loadable/component"
@@ -11,10 +11,15 @@ const Header   = loadable(() => import('~user/components/header/Header'))
 const Footer   = loadable(() => import('~user/components/footer/Footer'))
 
 //Import static files
-import shortIcon   from '~user/static/images/temporary/logo.png'
+import shortIcon from '~user/static/images/temporary/logo.png'
+import imageOG   from '~user/static/images/OGimage.jpg'
 import "./App.css"
 
 const App = () => {
+    const location = useLocation()
+
+    const image = `https://aic.xutd.tk${imageOG}`
+
     return (
         <>
             <Helmet
@@ -23,11 +28,18 @@ const App = () => {
             >
                 <meta name="viewport" content="width=device-width, minimum-scale=1.0"/>
                 <link rel="shortcut icon" href={shortIcon} />
+
+                <meta property="og:site_name" content="Гросс маркет"/>
+                <meta property="og:title" content="Наша суперцель — стать любимым магазином для каждой российской семьи"/>
+                <meta property="og:description" content="Сотни тысяч наших сотрудников ежедневно работают над достижением нашей суперцели. Мы уверены, что в ближайшие годы достигнем этого и будет здорово, если вместе с тобой." />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://aic.xutd.tk/" />
+                <meta property="og:image" content={image} />
             </Helmet>
 
                 <div className="page__body">
                     <Header theme={location.pathname === '/form' ? 'gray' : 'white'}/>
-                    <AnimatePresence exitBeforeEnter >
+                    <AnimatePresence exitBeforeEnter initial={false}>
                         <Switch location={location} key={location.pathname}>
                             <Route exact path="/"     component={HomePage} />
                             <Route       path="/form" component={FormPage} />
