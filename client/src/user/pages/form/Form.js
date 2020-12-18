4fed75c18c3b56685b0f2df869d7b81f5995cfe9
Cerberrus <genochka_kravchenko@mail.ru>
2020-12-19 01:53:04 +0300

    Refactored form page

--- client/src/user/pages/form/Form.js
+++ client/src/user/pages/form/Form.js
@@ -1,13 +1,14 @@
 import React, {Component} from "react"
-import Helmet from "react-helmet"
-import NumberFormat from 'react-number-format'
-import axios from "axios"
-import { motion } from "framer-motion"
-
-import Success from "./components/success/Success"
-import Goal    from "./components/goal/Goal"
+import NumberFormat from "react-number-format"
+import { motion }   from "framer-motion"
+import Helmet    from "react-helmet"
 import ReCAPTCHA from "react-google-recaptcha"
 
+// Import components
+import Success from "./components/success/Success"
+import Goal    from "./components/goal/Goal"
+
+// Import model
 import model from "~src/model/model"
 
 // Import static files
@@ -93,7 +94,6 @@ export default class Form extends Component {
 
     onSubmit = async (e) => {
         e.preventDefault()
-        const { vacancy, fullName, date, sex, phone, mail, resume, file, captcha, agreement } = this.state.formData
 
         const requiredFields = ['vacancy', 'fullName', 'date', 'phone']
         let   error, result
@@ -117,7 +117,7 @@ export default class Form extends Component {
                 error = true
 
                 input.classList.add('form__input_error')
-                input.insertAdjacentHTML('afterend', '<span class="form__errorText">поле заполнено не корректно</span>');
+                input.insertAdjacentHTML('afterend', '<span class="form__errorText">поле заполнено не корректно</span>')
 
                 input.addEventListener('focus', () => {
                     input.classList.remove('form__input_error')
@@ -126,52 +126,34 @@ export default class Form extends Component {
             }
         })
 
-        if (!error && agreement) {
-            axios({
-                method: 'post',
-                headers: {
-                    'Content-Type': file ? 'multipart/form-data' : 'application/json'
-                },
-                url: `https://aic.xutd.tk/api/summary?`+
-                     `g-recaptcha-response=${captcha}&`+
-                     `jobVacancyId=${vacancy}&`+
-                     `name=${fullName}&`+
-                     `happyDate=${date}&`+
-                     `phoneNumber=${phone}&`+
-                     `${sex && 'sex='+sex+'&'}`+
-                     `${mail && 'email='+mail+'&'}`+
-                     `${resume && 'resumeText='+resume}`
-                ,
-                data: file
-            }).then((response)=>{
-                console.log(response)
-                this.setState({
-                    success: true
+        const { formData } = this.state
+
+        if (!error && formData.agreement) {
+            this.model.postFormData(formData)
+                .then((response) => {
+                    if (response.status === 200) {
+                        this.setState({
+                            success: true
+                        })
+                    }
                 })
-            }).catch((error)=>{
-                console.log(error)
-                alert('Ошибка!')
-            })
         }
     }
 
     verifyCaptcha = async (response) => {
-        let formData  = this.state.formData
+        let formData = this.state.formData
         formData.captcha = response
-
         this.setState({formData})
     }
 
     onLoadFile = (e) => {
         this.validation(e)
-
         const fileLabel = e.target.labels[0].querySelector('.resumeFile__text')
         fileLabel.innerHTML = e.target.files[0].name
     }
 
     render() {
-        const { vacancyList } = this.state
-        const { success } = this.state
+        const { vacancyList, success } = this.state
         const { vacancy, fullName, date, phone, mail, resume } = this.state.formData
 
         const IconChecked = <svg className="form__iconCheck" aria-hidden={true}><use xlinkHref={iconCheck}/></svg>
@@ -351,15 +333,12 @@ export default class Form extends Component {
 
         return (
             <>
-                <Helmet>
-                    <title>анкета</title>
-                    <meta name="description" content="Form page" />
-                </Helmet>
+                <Helmet title="анкета" />
 
                 <motion.main
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
-                    exit={{  opacity: 0 }}
+                    exit={{ opacity: 0 }}
                     transition={{delay: 0.2}}
                     className="requestPage container"
                 >
