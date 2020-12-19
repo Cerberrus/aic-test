aa8ba68d3038dff507fdf72167cfd4be3dc862b4
Cerberrus <genochka_kravchenko@mail.ru>
2020-12-20 00:53:22 +0300

    Updated logo, favicon and og images

--- client/src/user/components/logo/Logo.js
+++ client/src/user/components/logo/Logo.js
@@ -5,16 +5,15 @@ import { Link } from "react-router-dom"
 import './Logo.css'
 
 //Temporary
-import logo from '~user/static/images/temporary/logo.png'
+import logo from '~user/static/images/logo.png'
 
-const Logo = () => {
-    return (
-        <Link to="/" title="Перейти на главную" className="logo">
-            <img src={logo} alt="" className="logo__image"/>
-            <h1 className="logo__name">гросс маркет</h1>
-        </Link>
-    )
-}
+const Logo = () => (
+    <Link to="/" title="Перейти на главную" className="logo">
+        <img src={logo} alt="" className="logo__image"/>
+        <h1 className="logo__name">гросс маркет</h1>
+    </Link>
+)
+
 
 export default Logo
 
