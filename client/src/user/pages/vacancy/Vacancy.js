dec74cb738db38db1543dde152692aa0389eb95c
Cerberrus <genochka_kravchenko@mail.ru>
2020-12-21 03:25:00 +0300

    Splited vacancy page to two components (form and goal)

--- client/src/user/pages/vacancy/Vacancy.js
+++ client/src/user/pages/vacancy/Vacancy.js
@@ -1,50 +1,39 @@
-import React, {Component} from "react"
-import NumberFormat from "react-number-format"
+import React, { Component } from "react"
 import { AnimatePresence, motion } from "framer-motion"
-import Helmet    from "react-helmet"
-import ReCAPTCHA from "react-google-recaptcha"
+import Helmet from "react-helmet"
 
 // Import components
-import Loader    from '~user/components/loader/Loader'
-import Header from '~user/components/header/Header'
-import Footer from '~user/components/footer/Footer'
-import Success from "./components/success/Success"
+import Loader from "~user/components/loader/Loader"
+import Header from "~user/components/header/Header"
+import Footer from "~user/components/footer/Footer"
+import Form    from "./components/form/Form"
 import Goal    from "./components/goal/Goal"
+import Success from "./components/success/Success"
 
 // Import model
 import model from "~src/model/model"
 
 // Import static files
-import './Vacancy.css'
-import iconCheck from '~user/static/icons/check.svg'
-import iconFile from '~user/static/icons/clip.svg'
+import "./Vacancy.css"
 
 export default class Vacancy extends Component {
     model = new model()
 
     state = {
-        vacancyList: [],
-        formData: {
-            vacancy:   '',
-            fullName:  '',
-            date:      '',
-            sex:       'm',
-            phone:     '',
-            mail:      '',
-            resume:    '',
-            file:      '',
-            agreement: true,
-            captcha:   ''
-        },
-        error:         false,
-        loading:        true,
-        success:       false,
+        loading:  true,
+        success: false,
+        phone:      "",
     }
 
     componentDidMount() {
         window.scrollTo(0, 0)
-        this.model.getAllVacancies()
-            .then(vacancyList => this.setState({vacancyList}))
+
+        this.model.getInformation()
+            .then(response => {
+                this.setState({
+                    phone: response.phone
+                })
+            })
 
         setTimeout(() => {
             this.setState({
@@ -53,293 +42,14 @@ export default class Vacancy extends Component {
         }, 800)
     }
 
-    validation = (e) => {
-        const key  = e.target.name
-        let value  = e.target.value
-        let result = false
-
-        const checkName   = /./
-        const checkResume = /./
-        const checkDate   = /^(\d{4})[\s.\/-](\d{2})[\s.\/-](\d{2})$/
-        const checkPhone  = /^(\+7[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
-        const checkMail   = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
-
-        switch (key) {
-            case 'vacancy':
-                result = value !== '-1'
-                break
-            case 'fullName':
-                result = checkName.test(value)
-                break
-            case 'date':
-                result = checkDate.test(value)
-                break
-            case 'phone':
-                result = checkPhone.test(value)
-                break
-            case 'mail':
-                result = checkMail.test(value)
-                break
-            case 'resume':
-                result = checkResume.test(value)
-                break
-            case 'file':
-                value = new FormData()
-                value.append('summary', e.target.files[0])
-                result = true
-                break
-            case 'agreement':
-                value = e.target.checked
-                result = true
-                break
-            default:
-                break
-        }
-
-        let formData  = this.state.formData
-        formData[key] = result ? value : false
-
-        this.setState({formData})
-    }
-
-    onSubmit = async (e) => {
-        e.preventDefault()
-
-        const requiredFields = ['vacancy', 'fullName', 'date', 'phone']
-        let   error, result
-
-        requiredFields.map((field) => {
-           const input = document.querySelector(`[name=${field}]`)
-
-            // Validation
-            switch (field) {
-               case 'phone':
-                   const checkPhone = /^(\+7[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/
-                   result = checkPhone.test(input.value)
-                   break
-               default:
-                   result = input.value.length > 0 && input.value !== '-1'
-                   break
-            }
-
-            // Show errors
-            if (result === false) {
-                error = true
-
-                input.classList.add('form__input_error')
-                input.insertAdjacentHTML('afterend', '<span class="form__errorText">поле заполнено не корректно</span>')
-
-                input.addEventListener('focus', () => {
-                    input.classList.remove('form__input_error')
-                    input.nextElementSibling.remove()
-                })
-            }
+    onSuccess = () => {
+        this.setState({
+            success: true
         })
-
-        const { formData } = this.state
-
-        if (!error && formData.agreement) {
-            this.model.postFormData(formData)
-                .then((response) => {
-                    if (response.status === 200) {
-                        this.setState({
-                            success: true
-                        })
-                    }
-                })
-        }
-    }
-
-    verifyCaptcha = async (response) => {
-        let formData = this.state.formData
-        formData.captcha = response
-        this.setState({formData})
-    }
-
-    onLoadFile = (e) => {
-        this.validation(e)
-        const fileLabel = e.target.labels[0].querySelector('.resumeFile__text')
-        fileLabel.innerHTML = e.target.files[0].name
     }
 
     render() {
-        const { vacancyList, success, loading } = this.state
-        const { vacancy, fullName, date, phone, mail, resume } = this.state.formData
-
-        const IconChecked = <svg className="form__iconCheck" aria-hidden={true}><use xlinkHref={iconCheck}/></svg>
-
-        const Form = (
-            <>
-                <h1 className="visuallyHidden">Форма заявки</h1>
-                <p className="requestPage__title">Работа твоей мечты</p>
-
-                <div className="requestPage__content">
-                    {/* Form */}
-                    <form className="requestPage__form form" onSubmit={this.onSubmit} noValidate>
-                        {/* Vacancy */}
-                        <label>
-                            <span>Вакансия * {vacancy && IconChecked}</span>
-                            <select
-                                name="vacancy"
-                                className="form__select form__input"
-                                onChange={this.validation}
-                            >
-                                <option value={-1} defaultValue>Выберите вакансию</option>
-                                {vacancyList.map((vacancy, index) => (
-                                    <option key={index} value={vacancy.id}>
-                                        {vacancy.title}
-                                    </option>
-                                ))}
-                            </select>
-                        </label>
-
-                        {/* Full name */}
-                        <label>
-                            <span>ФИО * {fullName && IconChecked}</span>
-                            <input
-                                type="text"
-                                name="fullName"
-                                placeholder="Ваши данные"
-                                className="form__input"
-                                onChange={this.validation}
-                            />
-                        </label>
-
-                        {/* Date, sex, phone, mail */}
-                        <div className="requestPage__formGroup">
-                            {/* Date */}
-                            <label>
-                                <span>Дата рождения * {date && IconChecked}</span>
-                                <input
-                                    type="date"
-                                    name="date"
-                                    placeholder="28.07.2002"
-                                    className="form__input"
-                                    onChange={this.validation}
-                                />
-                            </label>
-
-                            {/* Sex */}
-                            <div>
-                                <p>Пол {IconChecked}</p>
-                                <ul className="requestPage__radioGroup form__radioGroup">
-                                    <li className="radioGroup__item">
-                                        <input
-                                            type="radio"
-                                            id="sex_m"
-                                            name="sex"
-                                            value="m"
-                                            onChange={this.validation}
-                                            defaultChecked
-                                            hidden
-                                        />
-                                        <label className="radioGroup__radio form__radio" htmlFor="sex_m">
-                                            <span>мужской</span>
-                                        </label>
-                                    </li>
-                                    <li className="radioGroup__item">
-                                        <input
-                                            type="radio"
-                                            id="sex_f"
-                                            name="sex"
-                                            value="f"
-                                            hidden onChange={this.validation}
-                                        />
-                                        <label className="radioGroup__radio form__radio" htmlFor="sex_f">
-                                            <span>женский</span>
-                                        </label>
-                                    </li>
-                                </ul>
-                            </div>
-
-                            {/* Phone */}
-                            <label>
-                                <span>Контактный телефон * {phone && IconChecked}</span>
-                                <NumberFormat
-                                    name="phone"
-                                    format="+7 (###) ### - ####"
-                                    mask="_" allowEmptyFormatting
-                                    className="form__input"
-                                    onChange={this.validation}
-                                />
-                            </label>
-
-                            {/* Mail */}
-                            <label>
-                                <span>Электронная почта {mail && IconChecked}</span>
-                                <input
-                                    type="email"
-                                    name="mail"
-                                    placeholder="example@mail.com"
-                                    className="form__input"
-                                    onChange={this.validation}
-                                />
-                            </label>
-                        </div>
-
-                        {/* Resume, resume file */}
-                        <div>
-                            {/* Resume */}
-                            <label>
-                                <span>Резюме {resume && IconChecked}</span>
-                                <textarea
-                                    name="resume"
-                                    className="requestPage__resume form__input"
-                                    onChange={this.validation}
-                                />
-                            </label>
-
-                            {/* Resume file */}
-                            <div>
-                                <input
-                                    type="file"
-                                    id="resume_file"
-                                    name="file"
-                                    onChange={this.onLoadFile}
-                                    hidden
-                                />
-                                <label htmlFor="resume_file" className="resumeFile form__file form__input">
-                                    <svg className="resumeFile__icon"><use xlinkHref={iconFile}/></svg>
-                                    <p className="resumeFile__text">выберите или перетащите файл</p>
-                                </label>
-                            </div>
-                        </div>
-
-                        {/* Captcha */}
-                        <div className="reCaptcha">
-                            <p>Капча</p>
-                            <div className="reCaptcha__group">
-                                <ReCAPTCHA
-                                    sitekey="6LfOQ-4ZAAAAACOFvjKDgtEwPjLqX3CdCPgTbTpL"
-                                    onChange={this.verifyCaptcha}
-                                    className="reCaptcha__block"
-                                />
-                                <span className="reCaptcha__info">* поля для обязательного заполнения</span>
-                            </div>
-                        </div>
-
-                        {/* Agreement */}
-                        <div>
-                            <input
-                                type="checkbox"
-                                name="agreement"
-                                id="agreement2"
-                                onChange={this.validation}
-                                hidden
-                                defaultChecked/>
-                            <label className="requestPage__agreement form__checkbox" htmlFor="agreement2">
-                                <p className="requestPage__agreementText">я подтверждаю согласие на обработку персональных данных и принимаю условия рассмотрения обращений *</p>
-                            </label>
-                        </div>
-
-                        <button className="form__button button_gray">Отправить</button>
-                    </form>
-
-                    {/* Goal section */}
-                    <Goal />
-                </div>
-            </>
-        )
+        const { success, loading, phone } = this.state
 
         return (
             <>
@@ -350,21 +60,40 @@ export default class Vacancy extends Component {
                 </AnimatePresence>
 
                 <motion.div
-                    initial={{
-                        opacity: 0,
-                    }}
-                    animate={{
-                        opacity: 1,
-                    }}
-                    exit={{
-                        opacity: 0,
-                    }}
-                    transition={{delay: 0.1}}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{delay: 0.1 }}
                     className="page__body"
                 >
                     <Header theme="gray"/>
                     <main className="requestPage container">
-                       {success ? <Success /> : Form}
+                        {/* For toggle components */}
+                        <AnimatePresence>
+                            {success ? (
+                                <motion.div
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    transition={{delay: 0.2 }}
+                                    key="success"
+                                >
+                                    <Success phone={phone}/>
+                                </motion.div>
+                            ) : (
+                                <motion.div
+                                    exit={{ opacity: 0 }}
+                                    transition={{delay: 0.2 }}
+                                    key="form"
+                                >
+                                    <h1 className="visuallyHidden">Форма заявки</h1>
+                                    <p className="requestPage__title">Работа твоей мечты</p>
+                                    <div className="requestPage__content">
+                                        <Form onSuccess={this.onSuccess}/>
+                                        <Goal phone={phone}/>
+                                    </div>
+                                </motion.div>
+                            )}
+                        </AnimatePresence>
                     </main>
                     <Footer/>
                 </motion.div>
