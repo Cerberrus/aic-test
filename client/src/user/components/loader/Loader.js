480587ec12d584f41f29a6ed345c19711b1b0a04
Cerberrus <genochka_kravchenko@mail.ru>
2020-12-22 21:52:11 +0300

    Loaders update

--- client/src/user/components/loader/Loader.js
+++ client/src/user/components/loader/Loader.js
@@ -29,7 +29,7 @@ const Loader = () => (
                 }}
                 transition={{ duration: 0.6 }}
             >
-                <img src={logo}  className="loader__image" aria-hidden={true}/>
+                <img src={`${process.env.URL_BASE}/static/icons/logo.png`}  className="loader__image" aria-hidden={true}/>
             </motion.div>
     </motion.div>
 )
